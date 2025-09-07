f4f2eb1859172a977ad97997c58bf7044a9e58f2
datawithanand <anandsuresh05@gmail.com>
2025-09-07 13:17:20 +0000

    added question1.py

---
+++ questions1.py
@@ -0,0 +1,23 @@
+'''
+-- Topics covered
+
+variables, datatypes, operators, strings, lists, dictionaries, tuples, sets, input/output
+
+-- Easy
+
+Write a Python program that takes an integer input from the user and prints whether it is even or odd.
+Create a list of numbers from 1 to 10, reverse it, and print the reversed list.
+Write a program to replace all occurrences of "hello" with "hi" in the string "hello world, hello Python".
+
+-- Medium
+
+Create a dictionary where the keys are strings "a", "b", "c" and the values are lists of numbers. Print the second element from the list of key "a".
+Write a program to count how many times the number 5 appears in the tuple (1, 5, 2, 5, 3, 5, 4).
+Given a list of integers, remove duplicates using a set, then convert it back into a sorted list.
+
+-- Hard
+
+Take a string input from the user. Count how many vowels are in the string, and store the counts in a dictionary (e.g., {'a': 2, 'e': 1}).
+Write a program that takes a nested list (matrix) and prints the transpose. Example: [[1,2,3],[4,5,6]] → [[1,4],[2,5],[3,6]].
+Given two lists of numbers, return a set containing numbers that are in first list but not in second, and another set containing numbers that are common in both.
+'''
