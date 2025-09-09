116f263241292e21524b1ade0ae48bc0374be65a
datawithanand <anandsuresh05@gmail.com>
2025-09-09 00:26:05 +0000

    09-Sep-2025

---
+++ main.py
@@ -0,0 +1,7 @@
+'''
+⚡ Easy (warm-up)
+
+1 - Take a number input and print its binary form (bin()) and whether it is divisible by 3.
+2 - Given the string "Python\nRocks\tHard", print its length and also print it again without escape characters being interpreted (raw string).
+3- Create a tuple of numbers, unpack it into variables, and reverse those variables into a new tuple.
+'''
