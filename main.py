9fc270065ff1e807a1f10b00520039ceb06714d3
datawithanand <anandsuresh05@gmail.com>
2025-09-09 02:25:19 +0000

    09-09-2025_2

--- main.py
+++ main.py
@@ -5,3 +5,7 @@
 2 - Given the string "Python\nRocks\tHard", print its length and also print it again without escape characters being interpreted (raw string).
 3- Create a tuple of numbers, unpack it into variables, and reverse those variables into a new tuple.
 '''
+
+num = int(input("Enter a number: "))
+for i in range(num,0,-1):
+  print('*' * i)
