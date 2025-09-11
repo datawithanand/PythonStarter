0fdb4e08f4b9b2cd22e72af8d46e48ebcbecccf2
Anand <anandsuresh05@gmail.com>
2025-09-11 00:15:15 +0000

    11-09_01

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 # MEDIUM — combine topics and “real” cases
 '''
 1. `count_vowels(s)` — write a function that returns a dictionary with counts for each vowel (`a,e,i,o,u`) **case-insensitive**. Keys should be lowercase.
+
+  vowels ='aeiouAEIOU'
+  result = {}
+
+  def vowel_count(string):
+    for char in string:
+      if char.isalpha() and char in vowels:
+        result[char.lower()] = string.count(char)
+
+  vowel_count('hEllo')
+  print(f"The number of vowels in the given string is: {result}")
+
 2. Given `d = {'a':[1,2], 'b':[3]}`, safely print the 2nd element of key `'a'` and handle a missing key `'z'` without raising an error (use dict methods).
 3. Use the **walrus operator** to read integers repeatedly until the user enters `0`, and print the running sum when loop ends. (One-line `while` condition should use `:=`.)
 4. Implement `transpose(matrix)` for a rectangular 2D list using nested `for` loops (do NOT use `zip`). Example: `[[1,2,3],[4,5,6]] -> [[1,4],[2,5],[3,6]]`.
@@ -13,4 +25,6 @@
 11. Given `name` and `age`, print `"Name: <name>\tAge: <age>\n"` using an f-string and escape sequences.
 12. Show the difference between copying a list by reference and copying by slicing: modify the child list in both cases and print the parent list to demonstrate behavior.
 '''
-# 1. `count_vowels(s)` — write a function that returns a dictionary with counts for each vowel (`a,e,i,o,u`) **case-insensitive**. Keys should be lowercase.
+# 2. Given `d = {'a':[1,2], 'b':[3]}`, safely print the 2nd element of key `'a'` and handle a missing key `'z'` without raising an error (use dict methods).
+
+
