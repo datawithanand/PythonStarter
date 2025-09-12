7594087c5eaa397a7f6d6e443fe4d7c1ca8e01aa
Anand <anandsuresh05@gmail.com>
2025-09-12 01:49:18 +0000

    12-09_final

--- questions2.py
+++ questions2.py
@@ -142,3 +142,83 @@
     print(f"The sum of the numbers: {result}")
 '''
 
+##########################################################################################################################################################################################
+
+'''
+10. Write a function `transpose(matrix)` that returns the transpose of a 2D list (matrix). Do **not** use built-in libraries like NumPy. Example: `[[1,2,3],[4,5,6]] → [[1,4],[2,5],[3,6]]`
+
+    nested_list = [[1,2,3],[4,5,6]]
+    result = []
+
+    for value in range(0, len(nested_list[0])):
+      temp = []
+      for row in nested_list:
+          temp.append(row[value])
+      result.append(temp)
+
+    print(f"The final pair of the given nested list is: {result}")
+
+11. Write a function `find_vowel_count(s)` that returns a dictionary of vowel counts from a string (case-insensitive). Example: `"Anand"` → `{'a':2}`.
+
+    vowels = 'aeiou'
+
+    def find_vowels_count(s):
+        vowel_count = {}
+        for char in s.lower():
+            if char in vowels:
+                vowel_count[char] = vowel_count.get(char, 0) + 1
+        return vowel_count
+
+    result = find_vowels_count('Anand')
+    print(f"The vowel count of given string: {result}")
+
+
+12. Write a program that: Takes a list of numbers from the user. Prints the sum of only the even numbers. If the list is empty, print `"No numbers provided"`.
+
+    def sum_even_nums(lst):
+        sum_even_count = 0
+        if not lst:
+            return 'No numbers provided'
+        for num in lst:
+            if num %2 ==0 :
+                sum_even_count += num
+        return sum_even_count
+
+    result = sum_even_nums([])
+    print(f"The sum of even numbers list is: {result}")
+
+13. Write a function `find_common_elements(list1, list2)` that returns a sorted list of elements that appear in both lists, **without duplicates**. Example: `[1,2,3,3]` and `[2,3,4]` → `[2,3]`.
+
+    def find_common_elements(list1, list2):
+        result = []
+        for num in list1:
+            if num in list2:
+                if num not in result:
+                    result.append(num)
+        return sorted(result)
+
+    print(find_common_elements([1,2,3,3], [2,3,4]))
+
+14. Write a function `is_palindrome(s)` that checks if a string is a palindrome (ignores case and spaces). Example: `"Race car"` → `True`.
+
+    def is_palindrome(s):
+        return s.lower().replace(" ","") == s.lower().replace(" ","")[::-1]
+
+    string = "Race car"
+    result = is_palindrome(string)
+    print(result)
+
+15. (Scope + Functions) Write a nested function `outer()` that defines a variable `x = 10`. Inside it, define an `inner()` function that modifies `x` using `nonlocal`. Call both and print the modified `x`.
+
+    def outer_fun():
+        x = 10
+        print(f"x value before modified: {x}")
+        def inner_fun():
+            nonlocal x
+            x = 20
+            print(f"x value inside inner_fun: {x}")
+        inner_fun()
+
+    outer_fun()
+
+'''
