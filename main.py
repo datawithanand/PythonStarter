d78352ca3a750b964df6b3aafb5526f6ea9fcf79
Anand <anandsuresh05@gmail.com>
2025-09-14 01:24:42 +0000

    14-09_1

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-# 1. Write a function that returns the factorial of a number using both recursion and iteration.
-# adding
+# 2. Given a list of integers, return a dictionary with the number as the key and its frequency as the value.
+
+lst = [1,2,3,4,5,6,5,1,2,3,4,7,8,8,7,9]
+
+result = {}
+
+for num in lst:
+  if num in result:
+    result[num] += 1
+  else:
+    result[num] = 1
+
+print(f"The count of given list of numbers: {result}")
